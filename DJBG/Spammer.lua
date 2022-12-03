8998be3b02786802d17710514db3d9a6fb735cfd
NotHammer043 <107131733+NotHammer043@users.noreply.github.com>
2022-12-03 12:50:56 +0800

    Update Spammer.lua

--- DJBG/Spammer.lua
+++ DJBG/Spammer.lua
@@ -13,4 +13,7 @@ for i, player in pairs(Players:GetPlayers()) do
     game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("dsc.gg/djbg ~ Cheap cash.","All")
     wait(0.2)
     game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("dsc.gg/djbg ~ Cheapest jailbreak accounts on the market.","All")
+    ----- Teleport to a New server -----
+    wait(0.1)
+    Teleport()
 end

25a2a9dbe16a17e602a9e2e4f651acc9f6d0e391
NotHammer043 <107131733+NotHammer043@users.noreply.github.com>
2022-12-03 13:17:40 +0800

    Update Main.lua

--- DJBG/Main.lua
+++ DJBG/Main.lua
@@ -76,5 +76,5 @@ function Teleport()
 end
 
 ----- Spam Chat -----
-wait(20)
+wait(15)
 loadstring(game:HttpGet('https://raw.githubusercontent.com/NotHammer043/Random-Stuffs/main/DJBG/Spammer.lua'))()

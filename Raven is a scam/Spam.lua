a51c15c5eaf5373b80794079ac750058b572659d
NotHammer <107131733+NotHammer043@users.noreply.github.com>
2023-10-26 17:04:17 +0800

    Update Spam.lua

--- Raven is a scam/Spam.lua
+++ Raven is a scam/Spam.lua
@@ -2,7 +2,7 @@ task.spawn(function()
     repeat
         local ChatMessages = {
             " Expensive jailbreak cash at dsc.gg/djbg",
-            " Most scam and overpriced jailbreak cash at dsc.gg/djbg + Expensive robux foor 1.7x the price of the ones you can buy on adurite.com",
+            " Most scam and overpriced jailbreak cash at dsc.gg/djbg + Expensive robux for 1.7x the price of the ones you can buy on adurite.com",
         }
         game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(ChatMessages[ math.random( 1,#ChatMessages ) ], "All")
         task.wait(1.5)
